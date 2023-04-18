4294db92772323f7b4938d40c90a2cd21d55e68e
edem <edemadzav@gmail.com>
2023-04-18 15:07:42 +0000

    h gto

--- src/dummydata.js
+++ src/dummydata.js
@@ -1,86 +1,91 @@
-
-
 export const PHOTOS = [
   {
-    title : "Place de l'independance",
-    image : "https://toastmastersinscriptions.online/images/1672831583.jpg",
+    title: "Place de l'independance",
+    image: "https://toastmastersinscriptions.online/images/1672831583.jpg",
   },
   {
-    title : "Pure plage: Restaurant et bungalows",
-    image : "https://toastmastersinscriptions.online/images/1672834299.jpeg",
+    title: "Pure plage: Restaurant et bungalows",
+    image: "https://toastmastersinscriptions.online/images/1672834299.jpeg",
   },
   {
-    title : "Blue turtle bay",
-    image : "https://toastmastersinscriptions.online/images/1672834325.jpeg",
+    title: "Blue turtle bay",
+    image: "https://toastmastersinscriptions.online/images/1672834325.jpeg",
   },
   {
-    title : "Le Patio: Restaurant et Maison d'hôtes",
-    image : "https://toastmastersinscriptions.online/images/1672835411.jpeg",
+    title: "Le Patio: Restaurant et Maison d'hôtes",
+    image: "https://toastmastersinscriptions.online/images/1672835411.jpeg",
   },
   {
-    title : "Hôtel du 2 février",
-    image : "https://toastmastersinscriptions.online/images/1672835441.jpeg",
+    title: "Hôtel du 2 février",
+    image: "https://toastmastersinscriptions.online/images/1672835441.jpeg",
   },
   {
-    title : "Le patio: Restaurant et maison d'hôtes",
-    image : "https://toastmastersinscriptions.online/images/1672835521.jpeg",
+    title: "Le patio: Restaurant et maison d'hôtes",
+    image: "https://toastmastersinscriptions.online/images/1672835521.jpeg",
   },
   {
-    title : "Le farniente sur une plage de Lomé",
-    image : "/images/gallery/plagelome.jpeg",
+    title: "Le farniente sur une plage de Lomé",
+    image: "/images/gallery/plagelome.jpeg",
   },
   {
-    title : "Alt Muchen Restaurant",
-    image : "/images/gallery/AltMuchenRestaurant.jpeg",
+    title: "Alt Muchen Restaurant",
+    image: "/images/gallery/AltMuchenRestaurant.jpeg",
   },
   {
-    title : "Harmony Signature",
-    image : "/images/gallery/HarmonySignature.jpeg",
+    title: "Harmony Signature",
+    image: "/images/gallery/HarmonySignature.jpeg",
   },
   {
-    title : "Le patio hotel - restaurant",
-    image : "/images/gallery/LePatioHotelRestaurant.jpeg",
+    title: "Le patio hotel - restaurant",
+    image: "/images/gallery/LePatioHotelRestaurant.jpeg",
   },
   {
-    title : "Le patio Restaurant",
-    image : "/images/gallery/LePatioRestaurant.jpeg",
+    title: "Le patio Restaurant",
+    image: "/images/gallery/LePatioRestaurant.jpeg",
   },
   {
-    title : "Le patio suite junior",
-    image : "/images/gallery/LePatioSuiteJunior.jpeg",
+    title: "Le patio suite junior",
+    image: "/images/gallery/LePatioSuiteJunior.jpeg",
   },
   {
-    title : "Plage Lomé",
-    image : "/images/gallery/PlageLome.jpeg",
+    title: "Plage Lomé",
+    image: "/images/gallery/plagelome.jpeg",
   },
   {
-    title : "Pure plage",
-    image : "/images/gallery/pureplaisir.jpeg",
+    title: "Pure plage",
+    image: "/images/gallery/pureplaisir.jpeg",
   },
-]
-
-
+];
 
 export const rows = [
   {
     na: "1",
     name: "Hôtel 2 Février ( même endroit que le centre de conférence)",
     number: "200",
-    rooms: [{type: "Standard (double)", price: "65.000"}, {type: "Standard + (double)", price: "75.000"}],
+    rooms: [
+      { type: "Standard (double)", price: "65.000" },
+      { type: "Standard + (double)", price: "75.000" },
+    ],
     link: "	https://2fevrier.com/fr/",
   },
   {
     na: "2",
     name: "Hôtel Sarakawa (6,9 km du centre de conférence soit 13 min en voiture)",
     number: "100",
-    rooms: [{type: "Standard (double)", price: "91.000"}, { type: "tandard + (double)", price: "171.000"}],
+    rooms: [
+      { type: "Standard (double)", price: "91.000" },
+      { type: "tandard + (double)", price: "171.000" },
+    ],
     link: "https://sarakawa-hotel.com/",
   },
   {
     na: "3",
     name: "Hôtel Onomo (5,5 km du centre de conférence soit 11 min en voiture)",
     number: "58",
-    rooms:[{type: "Standard (double)", price: "49.510"}, { type:"Standard + (double)", price: "57.122" }],
+    rooms: [
+      { type: "Standard (double)", price: "49.510" },
+      { type: "Standard + (double)", price: "57.122" },
+    ],
     link: "https://www.booking.com/hotel/tg/onomo-lome.fr.html",
   },
 
@@ -88,22 +93,28 @@ export const rows = [
     na: "4",
     name: "	Hôtel ANHUI (2,7 km du centre de conférence soit 7 min en voiture)",
     number: "49",
-    rooms:[{type: "Standard (double)", price: "41.000"}, { type:"Standard + (double)", price: "47.000" }],
+    rooms: [
+      { type: "Standard (double)", price: "41.000" },
+      { type: "Standard + (double)", price: "47.000" },
+    ],
     link: "https://www.booking.com/hotel/tg/anhui.fr.html",
   },
- 
+
   {
     na: "5",
     name: "Hôtel Pélican (6,8 km du centre de conférence soit 13 min en voiture)",
     number: "30",
-    rooms:[{type: "Standard (double)", price: "48.000"}, { type:"Standard (single)", price: "37.000" }],
+    rooms: [
+      { type: "Standard (double)", price: "48.000" },
+      { type: "Standard (single)", price: "37.000" },
+    ],
     link: "https://www.hotel-pelican.com/",
   },
   {
     na: "6",
     name: "Hôtel du Golfe (1,4 km du centre de conférence soit 5 min en voiture)",
     number: "30",
-    rooms: [{type: "Standard (double)", price: "70.000"}],
+    rooms: [{ type: "Standard (double)", price: "70.000" }],
     link: "https://www.booking.com/hotel/tg/du-golfe.fr.html",
   },
 
@@ -111,14 +122,17 @@ export const rows = [
     na: "7",
     name: "Blue turtler (7,6 km du centre de conférence soit 16 min en voiture)",
     number: "14",
-    rooms: [{type: "Standard (double)", price: "100.000"}, {type: "Standard + (double)", price: "120.000"}],
+    rooms: [
+      { type: "Standard (double)", price: "100.000" },
+      { type: "Standard + (double)", price: "120.000" },
+    ],
     link: "http://blueturtletogo.com/",
   },
   {
     na: "8",
     name: "Hôtel AHODIKPE (1 km du centre de conférence soit 3 min en voiture)",
     number: "123",
-    rooms: [{type: "Standard (double)", price: "45.000"}],
+    rooms: [{ type: "Standard (double)", price: "45.000" }],
     link: "",
   },
 ];
@@ -131,15 +145,16 @@ export const PROGRAM = [
   {
     daytitle: "Jour 02",
     daydate: "20 Oct",
-  },{
+  },
+  {
     daytitle: "Jour 03",
     daydate: "21 Oct",
-  },{
+  },
+  {
     daytitle: "Jour 04",
     daydate: "22 Oct",
   },
-]
-
+];
 
 export const ENTRYPOINT = [
   {
@@ -148,8 +163,7 @@ export const ENTRYPOINT = [
   {
     title: "Arrivée par route",
   },
-]
-
+];
 
 export const HOTELSNAME = [
   {
@@ -194,8 +208,7 @@ export const HOTELSNAME = [
   {
     noms: "Hotel AHODIKPE Standard (double) 45.000 Fcfa",
   },
-]
-
+];
 
 export const HOTELSNAME1 = [
   {
@@ -210,7 +223,7 @@ export const HOTELSNAME1 = [
   {
     noms: "Hôtel la pointe des antilles 61.000 Fcfa",
   },
-]
+];
 
 export const HOTELSNAME2 = [
   {
@@ -225,4 +238,4 @@ export const HOTELSNAME2 = [
   {
     noms: "SCI ROSALIE'S Appartement de 2 Chambres 150.000 Fcfa",
   },
-]
+];
